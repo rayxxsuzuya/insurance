4f917b1f8c23c902c119d06db8a0d29298902903
rayxxsuzuya <56367629+rayxxsuzuya@users.noreply.github.com>
2020-09-24 15:28:38 +0300

    hover effects + js events + validator

--- js/script.js
+++ js/script.js
@@ -9,3 +9,35 @@ $(function() {
         autoplaySpeed: 2000,
       });
 });
+
+
+let tabs = document.querySelectorAll('.tabs');
+let contents = document.querySelectorAll('.contents');
+
+for (let x = 0; x < tabs.length; x++) {
+    for (let i = 0; i < tabs[x].children.length; i++) {
+        tabs[x].children[i].addEventListener('click', function(e) {
+            e.preventDefault();
+            for (let j = 0; j < tabs[x].children.length; j++) {
+                tabs[x].children[j].classList.remove('active');
+                contents[x].children[j].classList.remove('active');
+            }
+            tabs[x].children[i].classList.add('active');
+            contents[x].children[i].classList.add('active');
+        });
+    }
+}
+
+let button = document.querySelectorAll('.policy-arrow__wrapper');
+let show = document.querySelectorAll('.policy__show');
+
+console.log(button);
+
+
+for (let i = 0; i < button.length; i++) {
+  button[i].addEventListener('click', function(e) {
+    e.preventDefault();
+    this.classList.toggle('active');
+    show[i].classList.toggle('active');
+  });
+}
