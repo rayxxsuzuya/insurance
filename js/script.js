91170b78c8bd49801f27fe2660a977489995b6c4
rayxxsuzuya <56367629+rayxxsuzuya@users.noreply.github.com>
2020-09-24 13:45:39 +0300

    full stylized

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(function() {
+    $('.slider__inner').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+      });
+});
